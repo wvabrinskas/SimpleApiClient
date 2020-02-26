4314d4379b7a2c02e17ba05c497e8baf665efcbd
William Vabrinskas <wvabrinskas@gmail.com>
2020-02-26 11:51:31 -0500

    updated package file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SimpleApiClient",
+    platforms: [
+        .iOS(.v13),.macOS(.v10_15), .tvOS(.v13), .watchOS(.v5)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
